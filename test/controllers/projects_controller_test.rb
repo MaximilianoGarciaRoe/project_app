50d544ff9c9ae5e5c600c51beb8ed4c823a3cd70
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2025-05-27 00:06:53 -0400

    refactor: fix test

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,38 +1,48 @@
 require "test_helper"
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get projects_index_url
-    assert_response :success
+  setup do
+    @project = projects(:one)
   end
 
-  test "should get show" do
-    get projects_show_url
+  test "should get index" do
+    get projects_url
     assert_response :success
   end
 
   test "should get new" do
-    get projects_new_url
+    get new_project_url
+    assert_response :success
+  end
+
+  test "should create project" do
+    assert_difference('Project.count') do
+      post projects_url, params: { project: { name: 'New Project', description: 'Test Description' } }
+    end
+
+    assert_redirected_to project_url(Project.last)
+  end
+
+  test "should show project" do
+    get project_url(@project)
     assert_response :success
   end
 
   test "should get edit" do
-    get projects_edit_url
+    get edit_project_url(@project)
     assert_response :success
   end
 
-  test "should get create" do
-    get projects_create_url
-    assert_response :success
+  test "should update project" do
+    patch project_url(@project), params: { project: { name: 'Updated Project' } }
+    assert_redirected_to project_url(@project)
   end
 
-  test "should get update" do
-    get projects_update_url
-    assert_response :success
-  end
+  test "should destroy project" do
+    assert_difference('Project.count', -1) do
+      delete project_url(@project)
+    end
 
-  test "should get destroy" do
-    get projects_destroy_url
-    assert_response :success
+    assert_redirected_to projects_url
   end
 end
