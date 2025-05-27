e029999f4b8d17b8baebfcbab79261d0966ca1d8
Maximiliano Garcia Roe <mrgarcia1@miuandes.cl>
2025-05-26 23:38:19 -0400

    feat: imporve style app with new features boostrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def flash_class(name)
+    base_class = "alert alert-dismissible fade show"
+    type_class = case name.to_s
+    when "notice" then "alert-success"
+    when "alert", "error" then "alert-danger"
+    else "alert-#{name}"
+    end
+
+    [ base_class, type_class ].join(" ")
+  end
+
+  def flash_icon_class(name)
+    case name.to_s
+    when "notice" then "bi bi-check-circle-fill text-success"
+    when "alert", "error" then "bi bi-exclamation-triangle-fill text-danger"
+    end
+  end
 end
